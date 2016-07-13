cc0f6990b4e43c760ab94570fa4c632326e48787
discoNeko <jitan881@yahoo.co.jp>
2016-07-13 13:10:39 +0900

    add smooth move (demo)

--- main.js
+++ main.js
@@ -31,6 +31,8 @@
 
 	var emap = [];
 
+	var move_now = -1;
+
 	var row = 50;
 	var col = 50;
 	var map = [];
@@ -220,15 +222,26 @@
 		motionChar();
 
 		//onKeyCheck();
-		drawMap();
+		if(move_now!=-1){
+			drawMoveCalc();
+			//drawMoveChar();
+			moveDoneCheck();
+			//drawMoveEnemy();
+		}else{
+			drawMap();
+			drawChar();
+			drawEnemy();
+		}
+
+
+		
+
 		drawMenu();
-		drawChar();
-		drawEnemy();
 
 		//debug
-		ctx.fillStyle = '#fff';
-		ctx.fillText("move : ASDW",150,520);
-		ctx.fillText("attack : Enter",150,570);
+		//ctx.fillStyle = '#fff';
+		//ctx.fillText("move : ASDW",150,520);
+		//ctx.fillText("attack : Enter",150,570);
 
 		requestId = window.requestAnimationFrame(renderTitle); 
 	}
@@ -263,10 +276,280 @@
 		}
 	}
 */
-	function moveChar(mx,my){
+
+
+/*
+
+	//移動中の滑らかな移動を描画
+	function drawMoveMap(){
+		move_now += 10;
+		var dd = Math.floor(24*move_now/100);
+		var d = Math.floor(MainChara.dir/2) - 1;
+		var dx = [0,1,-1,0];
+		var dy = [-1,0,0,1];
+
+		var ax = 155;
+		var ay = 80;
+		ctx.fillStyle = '#070';
+		ctx.fillRect(ax,ay-55,w-2*ax+5,h-2*ay+5);
+
+		var sx = 165;
+		var sy = 35;
+		var cx = MainChara.x | 0;
+		var cy = MainChara.y | 0;
+		var mx = cx - 10;
+		var my = cy - 9;
+		//補正値
+		sx += dd*dx[d];
+		sy += dd*dy[d];
+		for(var i = 0; i < 21; i++){
+			for(var j = 0; j < 19; j++){
+				var pos = drawMoveMapLimitCheck(mx,my);
+				mx = pos.mx;
+				my = pos.my;
+				var xx = mx + i;
+				var yy = my + j;
+				//ループの最初と最後はタイルが見切れるので、補正を入れる
+				if(cx == xx && cy == yy){
+					ctx.fillStyle = '#a00';
+					ctx.drawImage(map_chip,0,0,32,32,(i-1)*25+sx,(j-1)*25+sy,24,24);
+				}else if(map[xx][yy]){
+					ctx.fillStyle = '#a0a';
+					ctx.drawImage(map_chip,0,0,32,32,(i-1)*25+sx,(j-1)*25+sy,24,24);
+				}else{
+					ctx.fillStyle = '#aa0';
+					ctx.drawImage(map_chip,0,0,32,32,(i-1)*25+sx,(j-1)*25+sy,24,24);
+					ctx.drawImage(map_chip,224,0,32,32,(i-1)*25+sx,(j-1)*25+sy,24,24);
+				}
+				//ctx.fillRect(i*25+sx,j*25+sy,24,24);
+			}
+		}
+	}
+*/
+
+	//移動中の滑らかな移動を描画
+	//描画位置を一括で計算して、map,chara,enemyそれぞれに値を渡す
+	function drawMoveCalc(){
+		move_now += 10;
+		var dd = Math.floor(24*move_now/100);
+		var d = Math.floor(MainChara.dir/2) - 1;
+		var dx = [0,1,-1,0];
+		var dy = [-1,0,0,1];
+
+		var ax = 155;
+		var ay = 80;
+		ctx.fillStyle = '#070';
+		ctx.fillRect(ax,ay-55,w-2*ax+5,h-2*ay+5);
+
+		var sx = 165;
+		var sy = 35;
+		var cx = MainChara.x | 0;
+		var cy = MainChara.y | 0;
+		var mx = cx - 10;
+		var my = cy - 9;
+		//補正値
+		var hx = dd*dx[d];
+		var hy = dd*dy[d];
+		for(var i = 0; i < 21; i++){
+			for(var j = 0; j < 19; j++){
+				var pos = drawMoveMapLimitCheck(mx,my);
+				mx = pos.mx;
+				my = pos.my;
+				var xx = mx + i;
+				var yy = my + j;
+				if(pos.hit){
+					
+				}else{
+					drawMoveMap(xx,yy,(i-1)*25+sx+hx,(j-1)*25+sy+hy);
+					//ループの最初と最後はタイルが見切れるので、補正を入れる
+				}
+			}
+		}
+
+		//タイル描画後にchara,enemyを描画
+		//charaが画面端に寄っているときは、
+		//タイルを移動させずにcharaを移動させる
+		for(var i = 0; i < 21; i++){
+			for(var j = 0; j < 19; j++){
+				var pos = drawMoveMapLimitCheck(mx,my);
+				mx = pos.mx;
+				my = pos.my;
+				var xx = mx + i;
+				var yy = my + j;
+				if(emap[xx][yy] != -1){
+					drawMoveEnemy(xx,yy,(i-1)*25+sx-2+hx,(j-1)*25+sy-15+hy);
+				}
+				if(cx == xx && cy == yy){
+					drawMoveChar((i-1)*25+sx-2,(j-1)*25+sy-15);
+				}
+			}
+		}
+	}
+
+	function drawMoveMapLimitCheck(mx,my){
+		var hit = false;
+		if(mx < 0){
+			mx = 0;
+			hit = true;
+		}
+		if(mx + 21 > 50){
+			mx = 29;
+			hit = true;
+		}
+		if(my < 0){
+			my = 0;
+			hit = true;
+		}
+		if(my + 19 > 50){
+			my = 31;
+			hit = true;
+		}
+		return {mx,my,hit};
+	}
+
+	function drawMoveMap(r,c,x,y){
+/*
+		if(MainChara.x == r && MainChara.y == c){
+			ctx.fillStyle = '#a00';
+			ctx.drawImage(map_chip,0,0,32,32,x,y,24,24);
+		}else 
+*/
+		if(map[r][c]){
+			ctx.fillStyle = '#a0a';
+			ctx.drawImage(map_chip,0,0,32,32,x,y,24,24);
+		}else{
+			ctx.fillStyle = '#aa0';
+			ctx.drawImage(map_chip,0,0,32,32,x,y,24,24);
+			ctx.drawImage(map_chip,224,0,32,32,x,y,24,24);
+		}
+	}
+/*
+
+	function drawMoveChar(){
+		var dir = MainChara.dir;
+
+		var posx = 0, posy = 0;
+		var sx = 165;
+		var sy = 35;
+		var cx = MainChara.x | 0;
+		var cy = MainChara.y | 0;
+		var mx = cx - 9;
+		var my = cy - 8;
+		for(var i = 0; i < 19; i++){
+			for(var j = 0; j < 17; j++){
+				var pos = drawMapLimitCheck(mx,my);
+				mx = pos.mx;
+				my = pos.my;
+				var xx = mx + i;
+				var yy = my + j;
+				if(cx == xx && cy == yy){
+					posx = i*25+sx-2;
+					posy = j*25+sy-15;
+					break;
+				}
+			}
+		}
+		var mot = Math.floor(MainChara.mwait / 100);
+		if(mot>2)mot = 1;
+		switch(dir){
+			case 2 : //down
+				ctx.drawImage(MainChara.img,20*mot,0,20,28,posx,posy,28,38);
+				break;
+			case 4 : //left
+				ctx.drawImage(MainChara.img,20*mot,28,20,28,posx,posy,28,38);
+				break;
+			case 6 : //right
+				ctx.drawImage(MainChara.img,20*mot,56,20,28,posx,posy,28,38);
+				break;
+			case 8 : //up
+				ctx.drawImage(MainChara.img,20*mot,84,20,28,posx,posy,28,38);
+				break;
+		}
+	}
+
+
+*/
+
+
+	function drawMoveChar(x,y){
+		var dir = MainChara.dir;
+		var mot = Math.floor(MainChara.mwait / 100);
+		if(mot>2)mot = 1;
+		switch(dir){
+			case 2 : //down
+				ctx.drawImage(MainChara.img,20*mot,0,20,28,x,y,28,38);
+				break;
+			case 4 : //left
+				ctx.drawImage(MainChara.img,20*mot,28,20,28,x,y,28,38);
+				break;
+			case 6 : //right
+				ctx.drawImage(MainChara.img,20*mot,56,20,28,x,y,28,38);
+				break;
+			case 8 : //up
+				ctx.drawImage(MainChara.img,20*mot,84,20,28,x,y,28,38);
+				break;
+		}
+	}
+
+/*
+
+	function drawMoveEnemy(){
+		var posx = 0, posy = 0;
+		var sx = 165;
+		var sy = 35;
+		var cx = MainChara.x | 0;
+		var cy = MainChara.y | 0;
+		var mx = cx - 9;
+		var my = cy - 8;
+		var mot = Math.floor(MainChara.mwait / 100);
+		if(mot>2)mot = 1;
+		for(var i = 0; i < 19; i++){
+			for(var j = 0; j < 17; j++){
+				var pos = drawMapLimitCheck(mx,my);
+				mx = pos.mx;
+				my = pos.my;
+				var xx = mx + i;
+				var yy = my + j;
+				if(emap[xx][yy]!=-1){
+					posx = i*25+sx-2;
+					posy = j*25+sy-15;
+					ctx.drawImage(Enemy[emap[xx][yy]].img,20*mot,0,20,28,posx,posy,28,38);
+				}
+			}
+		}
+	}
+	*/
+
+	function drawMoveEnemy(xx,yy,x,y){
+		var mot = Math.floor(MainChara.mwait / 100);
+		if(mot>2)mot = 1;
+		ctx.drawImage(Enemy[emap[xx][yy]].img,20*mot,0,20,28,x,y,28,38);
+	}
+
+	function moveDoneCheck(){
+		if(move_now>99){
+			move_now =- 1;
+			switch(MainChara.dir){
+				case 2 : MainChara.y++; break;
+				case 4 : MainChara.x--; break;
+				case 6 : MainChara.x++; break;
+				case 8 : MainChara.y--; break;
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+	function moveChar(mx,my,dir){
 		if(canMove(mx,my)){
-			MainChara.x += mx;
-			MainChara.y += my;
+			move_now = 0;
+			//MainChara.x += mx;
+			//MainChara.y += my;
 		}
 		moveEnemy();
 		scanEnemyPos();
@@ -285,11 +568,11 @@
 		console.log("move "+xx+" "+yy);
 		if(mx!=0 && (0>xx || xx>row-1)){
 			console.log("xx false");
-			v = false;
+			return false;
 		}
 		if(my!=0 && (0>yy || yy>col-1)){
 			console.log("yy false");
-			v = false;
+			return false;
 		}
 		if(collisionEnemy(xx,yy)){
 			v = false;
@@ -359,11 +642,12 @@
 	}
 
 	function drawMenu(){
-		var ax = 150;
-		var ay = 100;
+		var ax = 15;
+		var ay = 480;
 		ctx.fillStyle = '#ddd';
-		//ctx.fillRect(ax,ay-50,w-2*ax,h-2*ay);
-
+		ctx.fillRect(ax,ay,635,105);
+		ctx.fillRect(ax,25,130,445);
+		ctx.fillRect(ax+645,25,125,560);
 	}
 
 	function drawChar(){
@@ -456,7 +740,7 @@
 			case 6 : tx++; break;
 			case 8 : ty--; break;
 		}
-		if(-1<tx && tx<row-1 && -1<ty && ty<col-1 && emap[tx][ty]!=-1){
+		if(-1<tx && tx<row && -1<ty && ty<col && emap[tx][ty]!=-1){
 			appEffect();
 			Enemy[emap[tx][ty]].exist = false;
 		}
@@ -484,22 +768,22 @@
 		on_key[key] = true;
 		if(on_key[65]){
 			//a
-			moveChar(-1,0);
+			moveChar(-1,0,4);
 			modCharDir(4);
 		}
 		if(on_key[83]){
 			//s
-			moveChar(0,1);
+			moveChar(0,1,2);
 			modCharDir(2);
 		}
 		if(on_key[68]){
 			//d
-			moveChar(1,0);
+			moveChar(1,0,6);
 			modCharDir(6);
 		}
 		if(on_key[87]){
 			//w
-			moveChar(0,-1);
+			moveChar(0,-1,8);
 			modCharDir(8);
 		}
 		if(on_key[13]){
